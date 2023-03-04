e309e41d91bac5a09263497037902a2d1dd90b20
Vitaliy <68891398+VK-Vitaliy@users.noreply.github.com>
2023-03-04 14:32:10 +0300

    dz2 (#3)

---
+++ frontend/src/components/Menu.js
@@ -0,0 +1,22 @@
+function Menu() {
+    return (
+        <div className="Menu">
+            <ul>
+                <li>
+                    <a href='#'>TODO</a>
+                </li>
+                <li>
+                    <a href='#'>About us</a>
+                </li>
+                <li>
+                    <a href='#'>Contacts</a>
+                </li>
+
+            </ul>
+
+        </div>
+    );
+
+}
+
+export default Menu
